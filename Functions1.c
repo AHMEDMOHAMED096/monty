5862a34063f09430a6bfd64ea2a674eda64398cf
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-10-23 02:11:03 +0300

    betty style modification

--- Functions1.c
+++ Functions1.c
@@ -8,45 +8,45 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new_node;
-    char *arg;
-    int push_arg;
+	stack_t *new_node;
+	char *arg;
+	int push_arg;
 
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        printf("Error: malloc failed\n");
-        error_exit(stack);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		printf("Error: malloc failed\n");
+		error_exit(stack);
+	}
 
-    arg = strtok(NULL, "\n ");
-    if (arg != NULL && isdigit(arg))
-    {
-        push_arg = atoi(arg);
-    }
-    else
-    {
-        printf("line %d: usage: push integer\n", line_number);
-        error_exit(stack);
-    }
+	arg = strtok(NULL, "\n ");
+	if (arg != NULL && isdigit(arg))
+	{
+		push_arg = atoi(arg);
+	}
+	else
+	{
+		printf("line %d: usage: push integer\n", line_number);
+		error_exit(stack);
+	}
 
-    new_node->n = push_arg;
-    new_node->prev = NULL;
-    
-    if (queueMode == 1)
-    {
-        new_node->next = NULL;
-        if (*stack != NULL)
-            (*stack)->prev = new_node;
-        *stack = new_node;
-    }
-    else
-    {
-        new_node->next = *stack;
-        if (*stack != NULL)
-            (*stack)->prev = new_node;
-        *stack = new_node;
-    }
+	new_node->n = push_arg;
+	new_node->prev = NULL;
+
+	if (queueMode == 1)
+	{
+		new_node->next = NULL;
+		if (*stack != NULL)
+			(*stack)->prev = new_node;
+		*stack = new_node;
+	}
+	else
+	{
+		new_node->next = *stack;
+		if (*stack != NULL)
+			(*stack)->prev = new_node;
+		*stack = new_node;
+	}
 }
 
 /**
@@ -58,13 +58,13 @@ void push(stack_t **stack, unsigned int line_number)
 void pall(stack_t **stack, unsigned int line_number)
 {
 	(void) line_number;
-    stack_t *current_node = *stack;
+	stack_t *current_node = *stack;
 
-    while (current_node != NULL)
-    {
-        printf("%d\n", current_node->n);
-        current_node = current_node->next;
-    }
+	while (current_node != NULL)
+	{
+		printf("%d\n", current_node->n);
+		current_node = current_node->next;
+	}
 }
 
 /**
@@ -75,12 +75,12 @@ void pall(stack_t **stack, unsigned int line_number)
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL)
-    {
-        printf("line %d: can't pint, stack empty\n", line_number);
-        error_exit(stack);
-    }
-    printf("%d\n", (*stack)->n);
+	if (*stack == NULL)
+	{
+		printf("line %d: can't pint, stack empty\n", line_number);
+		error_exit(stack);
+	}
+	printf("%d\n", (*stack)->n);
 }
 
 /**
@@ -91,17 +91,17 @@ void pint(stack_t **stack, unsigned int line_number)
 
 void swap(stack_t **stack, unsigned int line_number)
 {
-    int temp;
+	int temp;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        printf("line %d: can't swap, stack too short\n", line_number);
-        error_exit(stack);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("line %d: can't swap, stack too short\n", line_number);
+		error_exit(stack);
+	}
 
-    temp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = temp;
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
 
 /**
@@ -112,15 +112,15 @@ void swap(stack_t **stack, unsigned int line_number)
 
 void pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp_node;
+	stack_t *temp_node;
 
-    if (*stack == NULL)
-    {
-        printf("line %d: can't pop an empty stack\n", line_number);
-        error_exit(stack);
-    }
+	if (*stack == NULL)
+	{
+		printf("line %d: can't pop an empty stack\n", line_number);
+		error_exit(stack);
+	}
 
-    temp_node = (*stack)->next;
-    free(*stack);
-    *stack = temp_node;
+	temp_node = (*stack)->next;
+	free(*stack);
+	*stack = temp_node;
 }
